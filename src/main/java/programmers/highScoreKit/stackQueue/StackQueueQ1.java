dfbc6058ff7b2f1661e590ea23a9dd5d6dc99cef
jhk920613 <926jhk@gmail.com>
2021-02-02 22:00:47 +0900

    프로그래머스 - 0202

--- src/main/java/programmers/highScoreKit/stackQueue/StackQueueQ1.java
+++ src/main/java/programmers/highScoreKit/stackQueue/StackQueueQ1.java
@@ -1,4 +1,44 @@
 package programmers.highScoreKit.stackQueue;
 
+import java.util.*;
+
+// 주식 가격
 public class StackQueueQ1 {
+
+    public static void main(String[] args) {
+
+        int[] prices = new int[]{1, 2, 3, 2, 3};
+
+        int[] answer = solution(prices);
+
+        // [4, 3, 1, 1, 0]
+        for(int ans : answer) {
+            System.out.println(ans);
+        }
+
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        Stack<Integer> stack;
+
+        for (int i = 0; i < prices.length; i++) {
+            stack = new Stack<>();
+            int price = prices[i];
+
+            for (int j = i + 1; j < prices.length; j++) {
+                stack.push(prices[j]);
+
+                if(price > prices[j]) {
+                    break;
+                }
+            }
+
+            answer[i] = stack.size();
+        }
+
+        return answer;
+    }
+
 }
