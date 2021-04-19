dfd15c4b08e9ebb85191edb38d9dc21726eedd9b
jhk920613 <926jhk@gmail.com>
2021-04-19 19:34:41 +0900

    프로그래머스 - 0419

--- src/main/java/programmers/level2/KakaoFriendsColoringBook.java
+++ src/main/java/programmers/level2/KakaoFriendsColoringBook.java
@@ -36,11 +36,7 @@ public class KakaoFriendsColoringBook {
             for (int r = 0; r < n; r++) {
                 if(!visit[c][r]) {
                     int result = move(m, n, c, r, picture[c][r], visit, picture);
-//                    System.out.println();
                     if(picture[c][r] != 0) {
-//                        System.out.println("영역: " + picture[c][r]);
-//                        System.out.println("사이즈: " + result);
-//                        System.out.println();
                         numberOfArea++;
                         maxSizeOfOneArea = Math.max(maxSizeOfOneArea, result);
                     }
@@ -54,25 +50,17 @@ public class KakaoFriendsColoringBook {
     }
 
     public int move(int m, int n, int c, int r, int value, boolean[][] visit, int[][] picture) {
+        if(!canMove(m, n, c, r, value, visit, picture)) {
+            return 0;
+        }
+
         int answer = 1;
         visit[c][r] = true;
-//        System.out.println("[" + c + "][" + r +"]");
 
-        if(canMove(m, n, c, r, 1, value, visit, picture)) {
-            answer += move(m, n, c-1, r, value, visit, picture);
-        }
-
-        if(canMove(m, n, c, r, 2, value, visit, picture)) {
-            answer += move(m, n, c+1, r, value, visit, picture);
-        }
-
-        if(canMove(m, n, c, r, 3, value, visit, picture)) {
-            answer += move(m, n, c, r-1, value, visit, picture);
-        }
-
-        if(canMove(m, n, c, r, 4, value, visit, picture)) {
-            answer += move(m, n, c, r+1, value, visit, picture);
-        }
+        answer += move(m, n, c-1, r, value, visit, picture);
+        answer += move(m, n, c+1, r, value, visit, picture);
+        answer += move(m, n, c, r-1, value, visit, picture);
+        answer += move(m, n, c, r+1, value, visit, picture);
 
         return answer;
     }
@@ -80,25 +68,11 @@ public class KakaoFriendsColoringBook {
 
     // c - 행
     // r - 열
-    public boolean canMove(int m, int n, int c, int r, int move, int value, boolean[][] visit, int[][] picture) {
-        if(move == 1) { //상
-            if(c-1 < 0 || visit[c-1][r] || picture[c-1][r] != value) {
-                return false;
-            }
-        } else if(move == 2) {  //하
-            if(c+1 >= m || visit[c+1][r] || picture[c+1][r] != value) {
-                return false;
-            }
-        } else if(move == 3) {  //좌
-            if(r-1 < 0 || visit[c][r-1] || picture[c][r-1] != value) {
-                return false;
-            }
-        } else {  //우
-            if(r+1 >= n || visit[c][r+1] || picture[c][r+1] != value) {
-                return false;
-            }
+    public boolean canMove(int m, int n, int c, int r, int value, boolean[][] visit, int[][] picture) {
+        if((0<=c && c < m) && (0<=r && r < n) && !visit[c][r] && value == picture[c][r]) {
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
